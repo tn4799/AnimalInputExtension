220bec4e719c0b00ccd7b32beafccf5d5697a25d
tn4799 <tn4799@gmail.com>
2022-05-14 02:49:10 +0200

    added error code mapping and implemented moving animals out of trailer

--- src/controllers/AnimalScreenTrailerStorage.lua
+++ src/controllers/AnimalScreenTrailerStorage.lua
@@ -5,6 +5,40 @@ AnimalScreenTrailerStorage = {
 		MOVE_TO_TRAILER = "shop_moveToTrailer",
 		CONFIRM_MOVE_TO_STORAGE = "shop_confirmMoveToProduction",
 		CONFIRM_MOVE_TO_TRAILER = "shop_doYouWantToMoveAnimalsToTrailer"
+	},
+	MOVE_TO_FARM_ERROR_CODE_MAPPING = {
+		[AnimalInputEvent.MOVE_SUCCESS] = {
+			text = "shop_movedToFarm",
+			warning = false
+		},
+		[AnimalInputEvent.MOVE_ERROR_NO_PERMISSION] = {
+			text = "shop_messageNoPermissionToTradeAnimals",
+			warning = true
+		},
+		[AnimalInputEvent.MOVE_ERROR_SOURCE_OBJECT_DOES_NOT_EXIST] = {
+			text = "shop_messageTrailerDoesNotExist",
+			warning = true
+		},
+		[AnimalInputEvent.MOVE_ERROR_TARGET_OBJECT_DOES_NOT_EXIST] = {
+			text = "shop_messageHusbandryDoesNotExist",
+			warning = true
+		},
+		[AnimalInputEvent.MOVE_ERROR_INVALID_CLUSTER] = {
+			text = "shop_messageInvalidCluster",
+			warning = true
+		},
+		[AnimalInputEvent.MOVE_ERROR_ANIMAL_NOT_SUPPORTED] = {
+			text = "shop_messageAnimalTypeNotSupported",
+			warning = true
+		},
+		[AnimalInputEvent.MOVE_ERROR_NOT_ENOUGH_SPACE] = {
+			text = "shop_messageNotEnoughSpaceAnimals",
+			warning = true
+		},
+		[AnimalInputEvent.MOVE_ERROR_NOT_ENOUGH_ANIMALS] = {
+			text = "shop_messageNotEnoughAnimals",
+			warning = true
+		}
 	}
 }
 AnimalScreenTrailerStorage_mt = Class(AnimalScreenTrailerStorage, AnimalScreenBase)
@@ -67,15 +101,34 @@ function AnimalScreenTrailerStorage:getSourceMaxNumAnimals(itemIndex)
 end
 
 function AnimalScreenTrailerStorage:applySource(itemIndex, numItems)
-	
+	local item = self.sourceItems[itemIndex]
+	local clusterId = item:getClusterId()
+	local errorCode = AnimalInputEvent.validate(self.trailer, self.storage, clusterId, numItems, self.trailer:getOwnerFarmId())
+
+	if errorCode ~= nil then
+		local data = AnimalScreenTrailerStorage.MOVE_TO_FARM_ERROR_CODE_MAPPING[errorCode]
+
+		self.errorCallback(g_i18n:getText(data.text))
+
+		return false
+	end
+
+	local text = g_i18n:getText(AnimalScreenTrailerStorage.L10N_SYMBOL.MOVE_TO_FARM)
+
+	self.actionTypeCallback(AnimalScreenBase.ACTION_TYPE_SOURCE, text)
+	g_messageCenter:subscribe(AnimalInputEvent, self.onAnimalMovedToStorage, self)
+	g_client:getServerConnection():sendEvent(AnimalInputEvent.new(self.trailer, self.storage, clusterId, numItems))
+
+	return true
 end
 
 function AnimalScreenTrailerStorage:onAnimalMovedToStorage(errorCode)
-	
-end
+	g_messageCenter:unsubscribe(AnimalInputEvent, self)
+	self.actionTypeCallback(AnimalScreenBase.ACTION_TYPE_NONE, nil)
 
-function AnimalScreenTrailerStorage:onAnimalMovedToTrailer(errorCode)
-	
+	local data = AnimalScreenTrailerStorage.MOVE_TO_FARM_ERROR_CODE_MAPPING[errorCode]
+
+	self.sourceActionFinished(data.isWarning, g_i18n:getText(data.text))
 end
 
 function AnimalScreenTrailerStorage:onAnimalsChanged(obj, clusters)
